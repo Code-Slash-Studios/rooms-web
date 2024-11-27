30466e2be6e89017783f1148dc97398b4b40aeed
David Richman <david.richman@type3solutions.us>
2024-11-26 23:41:45 -0500

    movin stuffs

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+export default function App() {
+    return (
+        <div className='App'>
+            {/**TODO temporary boilerplate */}
+            <header className='App-header'>
+                <nav>
+                    <a href="/today">Today</a>
+                    <a href=""></a>
+                </nav>
+            </header>
+            <main>
+                
+            </main>
+        </div>
+    )
+}
