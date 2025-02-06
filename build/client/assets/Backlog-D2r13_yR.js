8a3e977603be00f56a774183274268012a2c4da2
David Richman <david.richman@type3solutions.us>
2025-02-06 14:16:32 -0500

    remove build

--- build/client/assets/Backlog-D2r13_yR.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./jsx-runtime-56DGgGmo.js";const r=()=>e.jsx(e.Fragment,{children:e.jsx("h1",{children:"Here you can see all the events in the future"})});export{r as default};
--- build/client/assets/_index-BCqKqAax.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./jsx-runtime-56DGgGmo.js";import r from"./Today-8iKydMyP.js";import"./Event-DOZJ465C.js";function i(){return o.jsx(r,{})}export{i as default};
