3a0bd2d797a0e486e8b0b82a1080303786b16def
David Richman <david.richman@type3solutions.us>
2025-02-06 13:36:16 -0500

    fixing rooms list

--- app/api/room.tsx
+++ app/api/room.tsx
@@ -1,6 +1,6 @@
 let rooms = new Map<number, string> ()
-rooms.set(1, "W210")
-rooms.set(2, "W211")
+rooms.set(1, "Confrence Room")
+rooms.set(2, "W210")
 rooms.set(3, "W212")
 
 export function getRooms () {
