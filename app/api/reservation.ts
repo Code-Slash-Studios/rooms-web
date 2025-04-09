b7cff5c70f0333096af269b86505a7f2e476dc3d
David Richman <david.richman@type3solutions.us>
2025-04-08 22:30:56 -0400

    updating api calls #1

--- app/api/reservation.ts
+++ app/api/reservation.ts
@@ -1,10 +1,8 @@
 import { Reservation } from "~/models/reservation";
-import { apiURL } from "./config";
-
 
 export async function getAll(): Promise<Reservation[] | undefined> {
     return fetch(
-        `${apiURL}/reservations`,
+        `${process.env.apiURL!}/reservations`,
         ).then((response) => {
             return response.json().then((json) => {
                 return json.map((r: any) => Reservation.factory(r));
@@ -15,7 +13,7 @@ export async function getAll(): Promise<Reservation[] | undefined> {
 
 export async function getById(id: string) {
     return fetch(
-        `${apiURL}/reservation/${id}`,
+        `${process.env.apiURL!}/reservation/${id}`,
         ).then((response) => {
             return response.json().then((json) => {
                 return Reservation.factory(json);
@@ -26,7 +24,7 @@ export async function getById(id: string) {
 
 export async function post(reservation: Reservation) {
     return fetch(
-        `${apiURL}/reservation`,
+        `${process.env.apiURL!}/reservation`,
         {
             method: "POST",
             headers: {
