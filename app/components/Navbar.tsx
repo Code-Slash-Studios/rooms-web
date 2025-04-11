4851c63c156b003c866adaa9ff0b4366113e0a48
David Richman <david.richman@type3solutions.us>
2025-04-11 11:34:57 -0400

    add icon

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,8 +1,12 @@
 import { Link } from "@remix-run/react";
 import "./Navbar.css";
 
+
 export default function() {
     return <nav className="navbar">
+        <div className="navbar-logo">
+            <Link to="/"><img alt="CIS Rooms" src="./CISRooms.png"></img></Link>
+        </div>
         <ul className="navbar-links">
             <li><Link to="/">Rooms</Link></li>
             <li><Link to="/reservations">Reservations</Link></li>
