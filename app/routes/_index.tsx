4ef08c99cdde33884318e8dacd8d12e9de25778d
David Richman <david.richman@type3solutions.us>
2025-04-10 23:05:55 -0400

    I thnk I figured it out

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,9 +1,10 @@
 import { getRooms } from "~/api/room";
 import Rooms from "./rooms";
 
-export async function loader() {
-    let rooms = getRooms();
-    return {"rooms": rooms};
+export const loader = async () => {
+    const roomData = await getRooms();
+    console.log(roomData)
+    return {roomsData: roomData.map((r) => r.toJSON()), getError: undefined};
 }
 
 export default function Index() {
