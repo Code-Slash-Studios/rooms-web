e7223a9127d4afd2d9f974dec701020862378ec2
David Richman <david.c.richman@gmail.com>
2025-05-07 17:27:52 -0400

    amazing how much a comma can break

--- app/utils/datetime.tsx
+++ app/utils/datetime.tsx
@@ -45,7 +45,7 @@ export function sameDay(date1: Date, date2: Date) {
     return date1.getDate() === date2.getDate() && date1.getMonth() === date2.getMonth() && date1.getFullYear() === date2.getFullYear()
 }
 export function genDate(date: Date) {
-    return date.toLocaleDateString("en-US", {timeZone:"America/New_York",month:"short",day:"2-digit",})
+    return date.toLocaleDateString("en-US", {timeZone:"America/New_York",month:"short",day:"2-digit"})
 }
 export function genTime(datetime: Date, hour12: boolean = true) {
     return datetime.toLocaleTimeString("en-US", {timeZone:"America/New_York", hour:"numeric", minute:"2-digit", hour12:hour12})
